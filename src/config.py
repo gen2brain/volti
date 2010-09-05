809e7c474702e386ceacfed5a849de7c9c84d03a
gen2brain <gen2brain@74a80316-fde1-11de-a1eb-ffa25d41dba3>
2010-09-05 16:24:35 +0000

    change version

--- src/config.py
+++ src/config.py
@@ -22,7 +22,7 @@ class Config:
     def __init__(self):
         """ Constructor """
         self.app_name = "volti"
-        self.app_version = "0.2.2"
+        self.app_version = "0.2.3-svn"
         self.res_dir = None
         self.locale_dir = None
 
